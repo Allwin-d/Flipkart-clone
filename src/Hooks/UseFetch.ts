3242071cf0ec54397e030b6096fe02c961afb48a
Allwin <allwinselva7@gmail.com>
2025-12-11 00:45:50 +0530

    deleted some files

--- src/Hooks/UseFetch.ts
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-import type { ProductsResponse } from "../Types/types";
-
-const UseFetch = async (url: string) :Promise<ProductsResponse> => {
-  const response = await axios.get<ProductsResponse>(url);
-  return response;
-};
-
-export default UseFetch;
--- src/utils/CurrConv.ts
+++
@@ -1,4 +0,0 @@
-export const CurrConv = (val: number) => {
-  const Inr = Math.ceil(val) * 88;
-  return Inr;
-};
