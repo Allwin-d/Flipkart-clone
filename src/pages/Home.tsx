8d70724f6916075d9d0fd6def605659496c5bd83
Allwin <allwinselva7@gmail.com>
2025-12-17 01:34:18 +0530

    Added home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,43 @@
-const Home =()=>{
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import type { ApiResponse } from "../Types/ApiResponse";
+
+const Home = () => {
+  const API_URL = import.meta.env.VITE_PRODUCTS_API;
+  console.log("import.meta.env", API_URL);
+
+  const fetchProducts = async () => {
+    const data = await axios.get<ApiResponse>(API_URL);
+    return data;
+  };
+
+  const { data, isError, isLoading } = useQuery({
+    queryKey: ["Products"],
+    queryFn: fetchProducts,
+    // refetchInterval : we use this because after the specific time , it will refetch the data from the server , for eg :in the weather app , the weather updates for like every sec 
+  });
+
+  console.log("Data : ", data);
+
+  if (isLoading) {
     return (
-        <div>
+      <div>
+        <p>Loading ....</p>
+      </div>
+    );
+  }
 
-        </div>
-    )
-}
+  if (isError) {
+    return (
+      <div>
+        <p>Failed to fetch Data ....</p>
+      </div>
+    );
+  }
 
-export default Home ;
+  return <div>
+    
+  </div>;
+};
+
+export default Home;
