7e3caf6323eff5f0f89c20453ef6826b44023ee6
Allwin <allwinselva7@gmail.com>
2025-12-17 00:09:07 +0530

    implemented the UseFetch custom hook

---
+++ src/Hooks/useFetch.tsx
@@ -0,0 +1,32 @@
+import { useEffect, useState } from "react";
+import type { ApiResponse } from "../Types/ApiResponse";
+import axios from "axios";
+
+const useFetch = (url: string) => {
+  const [data, setData] = useState<ApiResponse | null>(null);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<boolean>(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setLoading(true);
+        setError(false);
+
+        const response = await axios.get<ApiResponse>(url);
+        setData(response.data);
+      } catch (err) {
+        console.error(err);
+        setError(true);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [url]);
+
+  return { data, loading, error };
+};
+
+export default useFetch;
