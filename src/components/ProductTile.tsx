ac56356bf1038c34f1fa9253243cf9d5fd4e386a
Allwin <allwinselva7@gmail.com>
2026-01-22 17:57:19 +0530

    --no-verify

--- src/components/ProductTile.tsx
+++ src/components/ProductTile.tsx
@@ -9,7 +9,11 @@ const ProductTile = ({ item }: ProductTileProps) => {
   return (
     <div>
       <div className="flex flex-col space-y-2 space-x-2 cursor-pointer border-2 mt-4  p-2 mr-4 bg-white">
-        <img src={item.images[0]} alt={item.title} />
+        <img
+          src={item.images[0]}
+          alt={item.title}
+          className="hover:scale-125 transition ease-out duration-150"
+        />
         <p className="text-center">{item.title}</p>
         <p className="text-center">From {currConveter(item.price)}</p>
       </div>
