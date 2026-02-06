61444763c8615fe699494f98233acd783ba5b9ab
Allwin <allwinselva7@gmail.com>
2026-02-06 15:12:38 +0530

    --no-verify

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { Routes, Route } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-
 import Home from "./pages/Home";
 import Navbar from "./components/Navbar";
 import ProductDetails from "./pages/ProductDetails";
