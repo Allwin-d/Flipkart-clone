4f4fa07995ffb6a089d1dc4cc94310dbff510dac
Allwin <allwinselva7@gmail.com>
2026-03-06 17:59:40 +0530

    Added routes in the app page

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,24 @@
-const App =()=>{
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Products from "./pages/Products";
+import ProductDetails from "./pages/ProductDetails";
+import Cart from "./pages/Cart";
+import Checkout from "./pages/Checkout";
+import About from "./pages/About";
+
+const App = () => {
   return (
     <div>
-
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/products" element={<Products />}></Route>
+        <Route path="/productDetails:/id" element={<ProductDetails />}></Route>
+        <Route path="/cart" element={<Cart />}></Route>
+        <Route path="/checkout" element={<Checkout />}></Route>
+        <Route path="/about" element={<About />}></Route>
+      </Routes>
     </div>
-  )
-}
+  );
+};
 
 export default App;
